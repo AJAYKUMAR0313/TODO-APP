38a27e2cd75bd89e101b39451274b552e0e199a5
Ajay Kumar <kkajaykumar0313@gmail.com>
2025-01-22 14:59:28 +0530

    FE added

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,6 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
-mongoose.connect(
-  "mongodb+srv://kkajaykumar0313:e7NYPAouisxaf5wl@cluster0.1hvre.mongodb.net/"
-);
+mongoose.connect(proccess.env.DB_URL);
 
 const todoSchema = mongoose.Schema({
   title: String,
